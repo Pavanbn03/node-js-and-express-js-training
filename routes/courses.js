e46efe0be9fb5118d3610cf696db7b1f22b8e15c
Pavanbn03 <pavanbn03@gmail.com>
2021-06-08 14:46:28 +0530

    mongodb integreted with crud operations

--- routes/courses.js
+++ routes/courses.js
@@ -1,67 +1,116 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const Joi = require("joi");
-let courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
+
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((err) => console.log("Error", err));
 
 const schema = Joi.object({
-  name: Joi.string().min(3).required(),
+  name: Joi.string().min(3),
+  author: Joi.string().min(3),
+  tags: Joi.array().items(Joi.string()),
+  isPublished: Joi.bool(),
+});
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
 });
 
-router.get("/", (req, res) => {
-  res.send(courses);
-});
+const Course = mongoose.model("Course", courseSchema);
 
-router.get("/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-
-  course ? res.send(course) : res.status(404).send("Not found");
-});
-
-router.post("/", (req, res) => {
-  const result = schema.validate(req.body);
+const createCourse = async (body) => {
+  const result = schema.validate(body);
   if (result.error) {
     res.status(400).send(result.error);
     return;
   }
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
+  const { name, author, tags, isPublished } = body;
+  const course = new Course({
+    name: name,
+    author: author,
+    tags: tags,
+    isPublished: isPublished,
+  });
+  const data = await course.save();
+  return data;
+};
+const getCourse = async () => {
+  const courses = await Course.find({})
+    // .find({ price: { $gt: 10, $lt: 20 } }) comparisson operators
+    // .find({ price: { $in: [10, 15, 20] } })
+    // .find({ name: /^Rea/i })
+    // .or([{ name: "Pavan" }, { isPublished: true }])
+    // .and([{ author: "BN" }])
+    .limit(10)
+    .sort({ name: -1 });
 
-  res.send(courses);
-});
-
-router.put("/:id", (req, res) => {
-  console.log("inside");
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  // .countDocuments(); // displays number od documents
+  return courses;
+};
+const updateCourse = async (id, body) => {
+  const courses = await getCourse();
+  const course = courses.find((c) => c.id === id);
   !course && res.status(404).send("Not found");
 
-  const result = schema.validate(req.body);
+  const result = schema.validate(body);
   if (result.error) {
     res.status(400).send(result.error);
     return;
   }
-
-  course.name = req.body.name;
-  res.send(courses);
-});
-
-router.delete("/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  const coursedb = await Course.findById(id);
+  coursedb.set({ ...body });
+  const data = await coursedb.save();
+  return data;
+};
+const removeCourse = async (id) => {
+  const courses = await getCourse();
+  const course = courses.find((c) => c.id === id);
 
   if (!course) {
     res.status(404).send("Not found");
     return;
   }
 
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  res.send(courses);
+  const data = await Course.deleteOne({ _id: id });
+  return data;
+};
+
+router.get("/", async (req, res) => {
+  const result = await getCourse();
+  res.send(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const result = await getCourse();
+  const course = result.find((c) => c.id === req.params.id);
+
+  course ? res.send(course) : res.status(404).send("Not found");
+});
+
+router.post("/", async (req, res) => {
+  const newCourse = await createCourse(req.body);
+  res.send(newCourse);
+});
+
+router.put("/:id", async (req, res) => {
+  const updatedCourse = await updateCourse(req.params.id, req.body);
+  res.send(updatedCourse);
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedData = await removeCourse(req.params.id);
+  res.send(deletedData);
 });
 
 module.exports = router;
