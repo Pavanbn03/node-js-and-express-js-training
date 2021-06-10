84b3a56942e2069f732f086b71fc57231728aa64
Pavanbn03 <pavanbn03@gmail.com>
2021-06-10 09:42:54 +0530

    frontend display list of courde

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [course, setCourse] = React.useState([]);
   React.useEffect(() => {
     getCourse();
-  });
+  }, []);
   const getCourse = () => {
     axios
       .get("http://localhost:5000/api/courses")
@@ -15,7 +15,29 @@ function App() {
       })
       .catch((err) => console.log("error", err));
   };
-  return <div className="App">{course.map((c) => c.name)}</div>;
+  return (
+    <div className="App">
+      {course.map((course, index) => {
+        return (
+          <div key={course._id}>
+            <h1>Name: {course.name}</h1>
+            <p>ID: {course._id}</p>
+            <p>Author: {course.author} </p>
+            <p>
+              isPublished : <code>{course.isPublished.toString()}</code>
+            </p>
+            <div>
+              {course.tags.map((tag) => (
+                <p key={tag}>{tag}</p>
+              ))}
+            </div>
+            <p>Date: {course.date}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
